242defb51ccfcdd821bcda3d09310b32e694e742
Thomas Deconinck <thomas.deconinck@colisweb.com>
2024-10-03 15:33:37 +0200

    update

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
   opts = {
     ensure_installed = {
       "rescript",
+      "gitignore",
+      "json",
       "gitignore",
       "css",
       "go",
@@ -13,8 +17,18 @@ return {
       "lua",
       "html",
       "markdown",
+      "markdown_inline",
       "yaml",
       "tsx",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
     },
   },
 }
--- .wezterm.lua
+++ .wezterm.lua
@@ -5,10 +5,10 @@ local config = wezterm.config_builder()
 
 config.window_close_confirmation = "NeverPrompt"
 config.window_padding = {
-  left = 2,
-  right = 2,
-  top = 0,
-  bottom = 0,
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
 }
 
 -- tab bar
@@ -18,25 +18,25 @@ config.use_fancy_tab_bar = false
 
 -- tmux status
 wezterm.on("update-right-status", function(window, _)
-  local SOLID_LEFT_ARROW = ""
-  local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
-  local prefix = ""
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
 
-  if window:leader_is_active() then
-    prefix = " " .. utf8.char(0x1f30a) -- ocean wave
-    SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-  end
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
 
-  if window:active_tab():tab_id() ~= 0 then
-    ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-  end -- arrow color based on if tab is first pane
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end -- arrow color based on if tab is first pane
 
-  window:set_left_status(wezterm.format({
-    { Background = { Color = "#b7bdf8" } },
-    { Text = prefix },
-    ARROW_FOREGROUND,
-    { Text = SOLID_LEFT_ARROW },
-  }))
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
 end)
 
 config.color_scheme = "Catppuccin Frappe"
@@ -44,79 +44,79 @@ config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 15.0
 config.window_decorations = "RESIZE"
 config.background = {
-  {
-    source = { Color = "#282c35" },
-    width = "100%",
-    height = "100%",
-    opacity = 0.90,
-  },
+	{
+		source = { Color = "#282c35" },
+		width = "100%",
+		height = "100%",
+		opacity = 0.90,
+	},
 }
 config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 2000 }
 config.keys = {
-  {
-    mods = "LEADER",
-    key = "t",
-    action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-  },
-  {
-    mods = "LEADER",
-    key = "\\",
-    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    mods = "LEADER",
-    key = "n",
-    action = wezterm.action.ActivateTabRelative(1),
-  },
-  {
-    mods = "LEADER",
-    key = "Escape",
-    action = wezterm.action.CloseCurrentPane({ confirm = true }),
-  },
-  {
-    mods = "LEADER",
-    key = "-",
-    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    mods = "LEADER",
-    key = "h",
-    action = wezterm.action.ActivatePaneDirection("Left"),
-  },
-  {
-    mods = "LEADER",
-    key = "j",
-    action = wezterm.action.ActivatePaneDirection("Down"),
-  },
-  {
-    mods = "LEADER",
-    key = "k",
-    action = wezterm.action.ActivatePaneDirection("Up"),
-  },
-  {
-    mods = "LEADER",
-    key = "l",
-    action = wezterm.action.ActivatePaneDirection("Right"),
-  },
-  {
-    mods = "LEADER",
-    key = "LeftArrow",
-    action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
-  },
-  {
-    mods = "LEADER",
-    key = "RightArrow",
-    action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
-  },
-  {
-    mods = "LEADER",
-    key = "DownArrow",
-    action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
-  },
-  {
-    mods = "LEADER",
-    key = "UpArrow",
-    action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
-  },
+	{
+		mods = "LEADER",
+		key = "t",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "\\",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "n",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "Escape",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
 }
 return config
