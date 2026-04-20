2f7438a7feb6de49eaa2821e17d5cc415fa3aae3
Thomas Deconinck <thomas.deconinck@colisweb.com>
2026-04-20 16:19:51 +0200

    tmux update

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
+-- Set tmux window title to nvim-{foldername} when inside tmux
+if vim.env.TMUX then
+  local folder = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+  vim.fn.system("tmux rename-window 'nvim " .. folder .. "'")
+  vim.api.nvim_create_autocmd("VimLeave", {
+    callback = function()
+      vim.fn.system("tmux set-window-option automatic-rename on")
+    end,
+  })
+end
+
 vim.filetype.add({
   extension = {
     mjml = "html",
